9897e1dbeec44b312363eb26c456603c95e4155d
Sura <nguyenminhnhut3607@gmail.com>
2024-08-22 00:43:20 +0700

    bai1 filed propoty cotructor

---
+++ Buoi3/Buoi3/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    public class Human
+    {
+        private string name;
+        private string place_of_birth;
+        private DateTime birthday;
+        public string Name { get => name; set => name = value; }
+        public string Place_of_birth { get => place_of_birth; set => place_of_birth = value; }
+        public DateTime Birthday { get => birthday; set => birthday = value; }
+
+        public Human(string name, string place_of_birth, DateTime birthday)
+        {
+            this.name = name;
+            this.place_of_birth = place_of_birth;
+            this.birthday = birthday;
+        }
+        public Human(Human human)
+        {
+            this.name = human.Name;
+            this.place_of_birth = human.Place_of_birth;
+            this.birthday = human.Birthday;
+        }
+
+        
+    }
+}
---
+++ Buoi3/Buoi3/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    public class Student : Human
+    {
+        private string id;
+        private float gpa;
+        public string Id { get => id; set => id = value; }
+        public float Gpa { get => gpa; set => gpa = value; }
+        public Student(string id, string name, string place_of_birth, DateTime birthday, float gpa) : base(name, place_of_birth, birthday)
+        {
+            this.id = id;
+            this.gpa = gpa;
+        }
+        public Student(Student student) : base(student)
+        {
+            this.id = student.Id;
+            this.gpa = student.Gpa;
+        }
+
+        
+    }
+}
